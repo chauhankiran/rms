3773564774735206f42e2c16173afcd1d4e08098
KC <kiranchauhan68@gmail.com>
2024-09-15 17:34:54 +0530

    Added errors support

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const createError = require("http-errors");
 const express = require("express");
 const morgan = require("morgan");
 
@@ -7,10 +8,27 @@ const app = express();
 app.set("view engine", "pug");
 
 // Middleware
-app.use(morgan("short"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res, next) => {
   res.render("index");
+});
+
+// 404 Error
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.title = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 app.listen(3000, () => {
