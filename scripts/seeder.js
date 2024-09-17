5c30b7edca971334840801c41afcb694396014b8
KC <kiranchauhan68@gmail.com>
2024-09-17 23:53:36 +0530

    Added migration and seed support

---
+++ scripts/seeder.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const fs = require("fs");
+const join = require("path").join;
+const postgres = require("postgres");
+
+const seed = async ({ sql, path, before = null }) => {
+  const seeders = fs.readdirSync(path);
+
+  for await (const seeder of seeders) {
+    before && before(seeder);
+
+    await sql.file(join(path, seeder));
+  }
+};
+
+// SQL
+const sql = postgres({
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
+
+seed({
+  sql,
+  path: join(__dirname, "..", "seeders"),
+  before: (name) => {
+    console.log("Seeding: ", name);
+  },
+})
+  .then(() => {
+    console.log("All good.");
+    process.exit(1);
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
---
+++ seeders/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, password) VALUES ('kai@doe.com', '123456');
+INSERT INTO users (email, password) VALUES ('jane@doe.com', '123456');
+INSERT INTO users (email, password) VALUES ('merry@doe.com', '123456');
