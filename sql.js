26bc0735846449d1b6a1bac16e498e26838fa3cc
KC <kiranchauhan68@gmail.com>
2024-09-22 11:02:11 +0530

    Adjusted datetime format

--- sql.js
+++ sql.js
@@ -9,10 +9,25 @@ const sql = postgres({
   debug: console.log,
   types: {
     date: {
-      to: 1184,
+      to: 1114,
       from: [1082, 1083, 1114, 1184],
       serialize: (x) => x,
-      parse: (x) => x,
+      parse: (x) => {
+        if (x) {
+          const d = new Date(x);
+          return d.toLocaleDateString(undefined, {
+            weekday: "short",
+            hour12: true,
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          });
+        }
+        return x;
+      },
     },
   },
 });
