6e6bb46914e9dbaa83632b21ae009ae9f2fe7929
KC <kiranchauhan68@gmail.com>
2024-09-24 23:14:14 +0530

    Added UI for dropdown and dummy mass actions

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,9 @@
+const dropdown = document.querySelector(".dropdown");
+dropdown.addEventListener("click", function (event) {
+  event.stopPropagation();
+  dropdown.classList.toggle("is-active");
+});
+
 function deleteUser() {
   if (confirm("Are you sure you want to delete this user?")) {
     document.deleteUserForm.submit();
@@ -9,6 +15,27 @@ function deleteUser() {
 function archiveUser() {
   if (confirm("Are you sure you want to change the user status?")) {
     document.archiveUserForm.submit();
+  } else {
+    return false;
+  }
+}
+
+function activeMultipleUsers() {
+  const checkboxes = document.querySelectorAll("input[name='userId']:checked");
+  const selectedUserIds = [];
+
+  checkboxes.forEach((checkbox) => {
+    selectedUserIds.push(checkbox.value);
+  });
+
+  const count = selectedUserIds.length;
+
+  if (count === 0) {
+    alert("Please select user(s) first.");
+    return;
+  }
+
+  if (confirm(`Are you sure you want to active ${count} selected user(s)?`)) {
   } else {
     return false;
   }
