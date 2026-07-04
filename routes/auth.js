f6c646f8e070c1dc1675f9f1060ea83a3d5769b0
Kiran <kc@marichi.dev>
2026-07-04 19:23:46 +0530

    added registration

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ router.post("/login", async (req, res, next) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-        req.flash("info", "Email and/or password required.");
+        req.flash("error", "Email and/or password required.");
         return res.redirect("/auth/login");
     }
 
@@ -38,7 +38,58 @@ router.post("/login", async (req, res, next) => {
 
         res.redirect("/");
     } catch (err) {
-        next(err)
+        next(err);
+    }
+});
+
+router.get("/register", (req, res) => {
+    return res.render("auth/register", {
+        title: "Register"
+    })
+});
+
+router.post("/register", async (req, res, next) => {
+    const { firstName, lastName, email, password, repeatPassword } = req.body;
+
+    if (!firstName || !lastName || !email || !password) {
+        req.flash("error", "All fields are required.");
+        return res.redirect("/auth/register");
+    }
+
+    if (password.length < 6) {
+        req.flash("error", "Password needs to be at least 6 characters long.");
+        return res.redirect("/auth/register");
+    }
+
+    if (password !== repeatPassword) {
+        req.flash("error", "Entered password don't match.");
+        return res.redirect("/auth/register");
+    }
+
+    try {
+        const user = await sql`
+            INSERT INTO users (
+                "firstName",
+                "lastName",
+                "email",
+                "password"
+            ) VALUES (
+                ${firstName},
+                ${lastName},
+                ${email},
+                ${password}
+            ) returning id; 
+        `.then(([x]) => x);
+
+        if (!user) {
+            req.flash("error", "Unable to create account. Contact admin.");
+            return res.redirect("/auth/login");
+        }
+
+        req.flash("info", "User created. Continue with login.");
+        return res.redirect("/auth/login");
+    } catch (err) {
+        next(err);
     }
 })
 
